f966cf441a9302e0f754f071f0ab39609008a0d6
iida-09 <k23008kk@aitech.ac.jp>
2024-12-23 13:41:11 +0900

    add class

--- backend/models.py
+++ backend/models.py
@@ -1,10 +1,11 @@
 from peewee import *
 from database import db
 
-id = AutoField() #id
-username = CharField(max_length=15)  #ユーザーネーム
-keyword = AutoField() #合言葉
-content = TextField() #会話内容
+class Models(Model):
+    id = AutoField() #id
+    username = CharField(max_length=15)  #ユーザーネーム
+    keyword = AutoField() #合言葉
+    content = TextField() #会話内容
 
 class Meta:
         database = db  # 使用するデータベースを指定
